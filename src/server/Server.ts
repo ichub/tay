7bb6af2fc09c9351b6584eb4e511898b93e24dea
Ivan Chub <ivansergchub@gmail.com>
2017-10-20 18:44:18 -0700

    only calculate the image metadata on the first load

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -13,6 +13,50 @@ export const jsDir = path.join(rootPath, "dist", "bundle");
 export const tayDir = path.join(rootPath, "images_of_tay");
 export const cssDir = path.join(rootPath, "css");
 
+let cachedImageMetadata = null;
+
+function getImageMetadata(): Promise<ITayInfo> {
+    if (cachedImageMetadata) {
+        return Promise.resolve(cachedImageMetadata);
+    }
+
+    return new Promise<ITayInfo>((resolve, reject) => {
+        fs.readdir(tayDir, (err, files) => {
+            if (err) {
+                console.log(err);
+                return reject(err);
+            }
+
+            files = files.filter(file => [".jpg", ".png"].indexOf(path.extname(file)) >= 0);
+
+            resolve(<ITayInfo> {
+                files: files.map(fileName => {
+                    const parts = fileName.split("x");
+
+                    const noseLocation = {
+                        x: parseInt(parts[0], 10),
+                        y: parseInt(parts[1], 10),
+                    };
+
+                    const dimensions = sizeOf(path.join(tayDir, fileName));
+
+                    return {
+                        url: "/images_of_tay/" + fileName,
+                        noseInPixels: noseLocation,
+                        sizeInPixels: dimensions,
+                        noseInFractions: {
+                            x: noseLocation.x / dimensions.width,
+                            y: noseLocation.y / dimensions.height
+                        }
+                    }
+                })
+            });
+        });
+    });
+}
+
+const imageMetadataPromise = getImageMetadata();
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(htmlDir, "index.html"));
 });
@@ -22,37 +66,8 @@ app.use("/images_of_tay", express.static(tayDir));
 app.use("/css", express.static(cssDir));
 
 app.get("/tay.json", (req, res, next) => {
-    fs.readdir(tayDir, (err, files) => {
-        if (err) {
-            console.log(err);
-            return next(err);
-        }
-
-        files = files.filter(file => [".jpg", ".png"].indexOf(path.extname(file)) >= 0);
-
-        res.send(<ITayInfo> {
-            files: files.map(fileName => {
-                const parts = fileName.split("x");
-
-                const noseLocation = {
-                    x: parseInt(parts[0], 10),
-                    y: parseInt(parts[1], 10),
-                };
-
-                const dimensions = sizeOf(path.join(tayDir, fileName));
-
-                return {
-                    url: "/images_of_tay/" + fileName,
-                    noseInPixels: noseLocation,
-                    sizeInPixels: dimensions,
-                    noseInFractions: {
-                        x: noseLocation.x / dimensions.width,
-                        y: noseLocation.y / dimensions.height
-                    }
-                }
-            })
-        });
-    });
+    imageMetadataPromise.then(data => res.send(data))
+        .catch(err => next(err));
 });
 
 app.use((err, req, res, next) => {
